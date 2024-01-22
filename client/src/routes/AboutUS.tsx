70e1ce81cf30acb264cb19577f167d502c58f30d
Kanurkar Prateek <53488723+KanurkarPrateek@users.noreply.github.com>
2024-01-22 21:31:43 +0530

    Added Frontend UI

---
+++ client/src/routes/AboutUS.tsx
@@ -0,0 +1,18 @@
+import { Header } from "./Main";
+
+export default function AboutUSPage() {
+  return (
+    <>
+      <Header />
+
+      <div className=" border-t pt-3">
+        <div className="max-w-xl mx-auto ">
+          <div className="max-w-xl mx-auto  text-2xl font-semibold  tracking-tight leading-tight">
+            About <span className="text-blue-600 ">Snowden</span>
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+}
